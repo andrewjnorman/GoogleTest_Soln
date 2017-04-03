09dafad28de952635654816fd782535d2466117e
andrewjnorman <andrewjnorman@live.com>
2017-04-03 01:02:54 +0100

    removed _API bfore maon

--- GoogleTest_Proj/GoogleTest_Proj.cpp
+++ GoogleTest_Proj/GoogleTest_Proj.cpp
@@ -5,7 +5,7 @@
 #include "gtest\gtest.h"
 #include "..\MyProject2\Class1.h"
 
-GTEST_API_ int main(int argc, char **argv) {
+int main(int argc, char **argv) {
 	printf("Running main() from gtest_main.cc\n");
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
